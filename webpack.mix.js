d8cdf2d7d6d38228b627d3f9c1365bc56246de34
Neo Mokoena <mokoena.n.a@gmail.com>
2017-04-25 17:40:46 +0200

    Finish Theme Setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/material-kit.scss','public/css')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .styles(['resources/assets/css/bootstrap.min.css'],'public/css/all.css');
+   .styles(['resources/assets/css/bootstrap.min.css',
+            'resources/assets/lib/smooth-div-scroll/css/smoothDivScroll.css'],'public/css/all.css');
 
 
