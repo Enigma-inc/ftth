9c55607f78d961714c196b7ff4a6fb6ea5a24e35
Neo Mokoena <mokoena.n.a@gmail.com>
2017-05-02 16:00:05 +0200

    finish theme setup

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <div class="copyright text-center">
+            &copy; 2017, Econet Lesotho - Designed by <a target="_blank" href="http://www.enigma.co.ls">Enigma</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/pricing/pricing.blade.php
@@ -0,0 +1,45 @@
+
+    <div class="title margin-top-5 margin-bottom-5">
+        <h2>FTTH Pricing</h2>
+    </div>
+
+    <!-- Tabs with icons on Card -->
+    <div class="card card-nav-tabs margin-top-10">
+        <ul class="nav nav-pills nav-justified" data-tabs="tabs">
+            <li class="active">
+                <a href="#profile" data-toggle="tab">
+                    <i class="material-icons">face</i>
+                    24 Months Contract packages
+                </a>
+            </li>
+            <li>
+                <a href="#messages" data-toggle="tab">
+                    <i class="material-icons">chat</i>
+                    Prepaid Offers
+                </a>
+            </li>
+
+        </ul>
+
+        <div class="content">
+            <div class="tab-content text-center">
+                <div class="tab-pane active" id="profile">
+                    <p> I will be the leader of a company that ends up being worth billions of dollars,
+                        because I got the answers. I understand culture. I am the nucleus. I think that’s a
+                        responsibility that I have, to push possibilities, to show people, this is the level
+                        that things could be at. I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things could be at. </p>
+                </div>
+                <div class="tab-pane" id="messages">
+                    <p> I think that’s a responsibility that I have, to push possibilities, to show people,
+                        this is the level that things could be at. I will be the leader of a company that
+                        ends up being worth billions of dollars, because I got the answers. I understand
+                        culture. I am the nucleus. I think that’s a responsibility that I have, to push
+                        possibilities, to show people, this is the level that things could be at.</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
