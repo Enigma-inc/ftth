4ffd9b7b7c8b441dec2f45174c365eca34f559f3
Neo Mokoena <mokoena.n.a@gmail.com>
2017-05-08 08:24:17 +0200

    Start Application

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,69 +1,30 @@
 <div class="section section-basic padding-top-0 padding-bottom-5">
     <div id="mixedContent">
         <div class="contentBox">
-            <div class="grid">
-                <figure class="effect-layla">
-                    <img src="{{url('images/slides/music-movies.jpg')}}" alt="img06"/>
-                    <figcaption>
-                        <h2>Download <span>music</span> <br> and <br><span>movies</span> faster</h2>
-                    </figcaption>
-                </figure>
-            </div>
+                            <img src="{{url('images/slides/music-movies.jpg')}}" alt="img06"/>
+
+        </div>
+        <div class="contentBox">
+                          <img src="{{url('images/slides/stream-tv.jpg')}}" alt="img06"/>
+
+        </div>
+        <div class="contentBox">
+                            <img src="{{url('images/slides/social-media.jpg')}}" alt="img06"/>
+
         </div>
         <div class="contentBox">
             <div class="grid">
-                <figure class="effect-layla">
-                    <img src="{{url('images/slides/stream-tv.jpg')}}" alt="img06"/>
-                    <figcaption>
-                        <h2>Stream <span>TV/video</span> <br> and make <br> <span>uninterrupted </span><br> video calls </h2>
-                        <p>e.g whatsapp, facebook, skype, tango etc</p>
-                    </figcaption>
-                </figure>
+                  <img src="{{url('images/slides/apps.jpg')}}" alt="img06"/>
+
             </div>
         </div>
         <div class="contentBox">
-            <div class="grid">
-                <figure class="effect-layla">
-                    <img src="{{url('images/slides/social-media.jpg')}}" alt="img06"/>
-                    <figcaption>
-                        <h2><span>Fast connectivity</span> <br> to <br> <span>social media</span> </h2>
-                        <p>Whatsapp, Twitter, Facebook and Instagram</p>
-                    </figcaption>
-                </figure>
-            </div>
+                                <img src="{{url('images/slides/home-automation.jpg')}}" alt="img06"/>
+
         </div>
         <div class="contentBox">
-            <div class="grid">
-                <figure class="effect-layla">
-                    <img src="{{url('images/slides/apps.jpg')}}" alt="img06"/>
-                    <figcaption>
-                        <h2><span>Download games</span> <br>and <br> <span>apps faster</span></h2>
-                    </figcaption>
-                </figure>
-            </div>
-        </div>
-        <div class="contentBox">
-            <div class="grid">
-                <figure class="effect-layla">
-                    <img src="{{url('images/slides/home-automation.jpg')}}" alt="img06"/>
-                    <figcaption>
-                        <h2 style="font-size: 1.5em;">Turn your home into a <span>smart home </span> <br>and <br> <span>remotely manage</span><br> your life</h2>
-                        <p>surveillance, appliance management etc</p>
-                    </figcaption>
-                </figure>
-            </div>
-        </div>
-        <div class="contentBox">
-            <div class="grid">
-                <figure class="effect-layla">
-                    <img src="{{url('images/slides/one-com.jpg')}}" alt="img06"/>
-                    <figcaption>
-                        <h2 style="font-size: 1.6em;"><span>Bundle <br> </span>communication services in one simple
-                            <br><span> connection</span> </h2>
-                        <p>such as your TV, broadband, telephone or any digital connection and many more</p>
-                    </figcaption>
-                </figure>
-            </div>
+                <img src="{{url('images/slides/one-com.jpg')}}" alt="img06"/>
+
         </div>
 
 
